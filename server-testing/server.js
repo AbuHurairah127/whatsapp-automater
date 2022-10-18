02d36b56b2dc81795c524f4c4c0a117afb6c5b1a
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-10-19 00:09:08 +0500

    error in scrolling

--- server-testing/server.js
+++ server-testing/server.js
@@ -6,9 +6,17 @@ const scrape = async (url) => {
     const page = await browser.newPage();
     await page.goto(url);
     console.log("found url");
+    await page.setViewport({
+      width: 1200,
+      height: 800,
+    });
+    // await page.waitForSelector("div [id='pane-side']");
+    // const sidePane = await page.$("div [id='pane-side']");
+    await autoScroll(page);
     setTimeout(async () => {
       await page.waitForSelector("span [title='Çhámbër öf Şečrets 😉🙂']");
       const target = await page.$("span [title='Çhámbër öf Şečrets 😉🙂']");
+
       console.log("found target");
       await target.click();
       console.log("target clicked");
@@ -23,4 +31,42 @@ const scrape = async (url) => {
     console.log("error", error);
   }
 };
-scrape("https://web.whatsapp.com/");
+scrape("https://www.youtube.com/");
+// const browser = await puppeteer.launch({
+//   headless: false
+// });
+// const page = await browser.newPage();
+// await page.goto('https://www.yoursite.com');
+// await page.setViewport({
+//   width: 1200,
+//   height: 800
+// });
+
+// await autoScroll(page);
+
+// await page.screenshot({
+//   path: 'yoursite.png',
+//   fullPage: true
+// });
+
+// await browser.close();
+// })();
+
+async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      var totalHeight = 0;
+      var distance = 100;
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight - window.innerHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}

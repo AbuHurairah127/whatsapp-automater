2649cceb67fa3ab2745391bb68df280b0810288e
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-10-18 21:50:42 +0500

    whatsapp single msg don

--- server-testing/server.js
+++ server-testing/server.js
@@ -3,13 +3,20 @@ const puppeteer = require("puppeteer");
 const scrape = async (url) => {
   try {
     const browser = await puppeteer.launch({ headless: false });
-    console.log("🚀 ~ file: server.js ~ line 5 ~ scrape ~ browser", browser);
     const page = await browser.newPage();
-    console.log("🚀 ~ file: server.js ~ line 7 ~ scrape ~ page", page);
     await page.goto(url);
-    console.log("Abc");
+    await page.waitForSelector("span [title='+92 321 8867019']");
+    const target = await page.$("span [title='+92 321 8867019']");
+    console.log("found span");
+    await target.click();
+    console.log("target tk");
+    const inp = await page.$(
+      "#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._2lMWa > div.p3_M1 > div > div.fd365im1.to2l77zo.bbv8nyr4.mwp4sxku.gfz4du6o.ag5g9lrv > p"
+    );
+    await inp.type("Testing magic");
+    await page.keyboard.press("Enter");
   } catch (error) {
     console.log("error");
   }
 };
-scrape("https://www.google.com/");
+scrape("https://web.whatsapp.com/");

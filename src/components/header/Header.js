00b398829351ed9f5fcf0b395c750b48b5f2bd93
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-10-24 21:20:31 +0500

    header style completed

---
+++ src/components/header/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { styles } from "./HeaderStyles";
+const Header = () => {
+  return <div style={styles.container}>Whatsapp Message Sender</div>;
+};
+
+export default Header;
---
+++ src/components/header/HeaderStyles.js
@@ -0,0 +1,13 @@
+export const styles = {
+  container: {
+    width: "100%",
+    height: 60,
+    backgroundColor: "#52b788",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+};

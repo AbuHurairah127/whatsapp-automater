94bcf2d1919bb112d610512a9f48517913fe14a8
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-10-25 21:31:16 +0500

    on Submit working but no conditions is added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,16 @@ const App = () => {
   });
   const onChangeHandler = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setData({ ...data, [e.target.name]: e.target.value });
     console.log(data);
+    for (let i = 0; i < data.message.length; i++) {
+      if (data.message[i] === " ") {
+        data.message[i] = "%20";
+      }
+    }
   };
   return (
     <div className="App">
@@ -29,47 +38,50 @@ const App = () => {
             flexDirection: "column",
           }}
         >
-          <textarea
-            style={{
-              marginTop: 25,
-              width: "50%",
-              outline: "none",
-              minHeight: 200,
-              fontFamily: "sans-serif",
-              borderRadius: 8,
-              padding: 5,
-            }}
-            type="text"
-            value={data.message}
-            name="message"
-            onChange={(e) => onChangeHandler(e)}
-            placeholder="Type your message here e.g Assalam u Alaikum! Abu Hurairah Here..."
-          />
-          <input
-            style={{
-              marginTop: 25,
-              width: "50%",
-              height: 40,
-              borderRadius: 8,
-              outline: "none",
-              padding: 5,
-              border: "1px solid black",
-            }}
-            type="number"
-            placeholder="Enter the whatsapp no."
-          />
-          <p
-            style={{
-              color: "red",
-            }}
-          >
-            Enter the number as 923207443537
-            <br />
-            Please Don't Enter as 03207443537 or +923207443537
-          </p>
-          <a href="">
+          <form onSubmit={(e) => onSubmitHandler(e)}>
+            <textarea
+              style={{
+                marginTop: 25,
+                width: "50%",
+                outline: "none",
+                minHeight: 200,
+                fontFamily: "sans-serif",
+                borderRadius: 8,
+                padding: 5,
+              }}
+              type="text"
+              value={data.message}
+              name="message"
+              onChange={(e) => onChangeHandler(e)}
+              placeholder="Type your message here e.g Assalam u Alaikum! Abu Hurairah Here..."
+            />
+            <input
+              style={{
+                marginTop: 25,
+                width: "50%",
+                height: 40,
+                borderRadius: 8,
+                outline: "none",
+                padding: 5,
+                border: "1px solid black",
+              }}
+              type="number"
+              name="phone"
+              value={data.phone}
+              onChange={(e) => onChangeHandler(e)}
+              placeholder="Enter the whatsapp no."
+            />
+            <p
+              style={{
+                color: "red",
+              }}
+            >
+              Enter the number as 923207443537
+              <br />
+              Please Don't Enter as 03207443537 or +923207443537
+            </p>
             <button
-              type="button"
+              type="submit"
               style={{
                 backgroundColor: "#1b4332",
                 color: "white",
@@ -83,7 +95,7 @@ const App = () => {
             >
               Send Message
             </button>
-          </a>
+          </form>
         </div>
       </main>
     </div>
